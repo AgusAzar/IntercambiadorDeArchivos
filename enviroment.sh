5c8759af8f38bd242ec23edd190d1fc06f463a6b
AgusAzar <agustin.azar101@gmail.com>
2020-12-03 21:56:51 -0300

    ver transacciones

---
+++ enviroment.sh
@@ -0,0 +1,1 @@
+export  ANDROID_SDK_ROOT=/opt/android-sdk PATH=${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:/opt/android-studio/bin
--- www/js/my-app.js
+++ www/js/my-app.js
@@ -2,6 +2,7 @@
 var usuarioEstaLogeado, userMail, userName;
 var db = firebase.firestore();
 var storage = firebase.storage();
+var lastTimeStamp = 0;
 var data = {
     nombre: "usuario 1",
     DISPOSITIVOS: ["Mi pc"],
@@ -20,19 +21,28 @@ var app = new Framework7({
     // Enable swipe panel
     panel: {
         swipe: "left",
-    },
-    // Add default routes
+    }, // Add default routes
     routes: [
         {
             path: "/main/",
             url: "main.html",
         },
-        { path: "/register/", url: "registro.html" },
+        {
+            path: "/register/",
+            url: "registro.html",
+        },
         {
             path: "/addContacto/",
             url: "addContacto.html",
         },
-        { path: "/contacto/:userId", url: "contacto.html" },
+        {
+            path: "/contacto/:userId",
+            url: "contacto.html",
+        },
+        {
+            path: "/transaccion/:transaccionId",
+            url: "transaccion.html",
+        },
     ],
     // ... other parameters
 });
@@ -99,6 +109,7 @@ $$(document).on("page:init", '.page[data-name="registro"]', function () {
 });
 $$(document).on("page:init", '.page[data-name="main"]', function () {
     getContactos();
+    getTransacciones();
 });
 
 $$(document).on("page:init", '.page[data-name="addContacto"]', function () {
@@ -149,16 +160,43 @@ $$(document).on("page:init", '.page[data-name="contacto"]', function () {
     });
     $$("#btnTransaccion").on("click", function () {
         var archivos = document.getElementById("archivos").files;
-        db.collection('TRANSACCIONES').add({
-            emisor: userMail,
-            receptor: contactId,
-            cantidad: archivos.length
-        }).then(function(docRef){
-            var storageRef = storage.ref(docRef.id);
-            Array.from(archivos).forEach((archivo) => {
-                var filerRef = storageRef.child(archivo.name);
-                filerRef.put(archivo);
-        })
+        db.collection("TRANSACCIONES")
+            .add({
+                usuarios: [userMail, contactId],
+                cantidad: archivos.length,
+                timeStamp: Date.now(),
+            })
+            .then(function (docRef) {
+                var storageRef = storage.ref(docRef.id);
+                Array.from(archivos).forEach((archivo) => {
+                    var filerRef = storageRef.child(archivo.name);
+                    filerRef.put(archivo);
+                });
+            });
+    });
+});
+$$(document).on("page:init", '.page[data-name="transaccion"]', function () {
+    var transaccionId = router.currentRoute.params.transaccionId;
+    $$(".fileId").on("click", function () {
+        var url = $$(this).attr("id");
+        downloadFile(url);
+    });
+    console.log(transaccionId);
+    var ref = storage.ref(transaccionId);
+    ref.listAll().then(function (res) {
+        res.items.forEach(function (itemRef) {
+            console.log(itemRef);
+            itemRef.getDownloadURL().then(function (url) {
+                console.log(url);
+                $$("#fileList").append(
+                    `<li><div class="item-content"><div class="item-inner"> <div class="item-title">` +
+                        itemRef.name +
+                        `</div> <a id="` +
+                        url +
+                        `" href="#" class="fileId f7-icons size-50">arrow_down_circle</a> </div> </div></li>`
+                );
+            });
+        });
     });
 });
 //$$(document).on("page:init", '.page[data-name="about"]', function (e) {});
@@ -203,7 +241,7 @@ function getContactos() {
             querySnapshot.forEach(function (doc) {
                 console.log(doc.id + " => " + doc.data());
                 $$("#listaDeContacto").append(
-                    '<li><a href="/contacto/' +
+                    '<li><a class="contacto" href="/contacto/' +
                         doc.id +
                         '">' +
                         doc.data().nombre +
@@ -216,4 +254,58 @@ function mostrarArchivos(files) {
     Array.from(files).forEach((archivo) => {
         $$("#fileList").append("<li>" + archivo.name + "</li>");
     });
+}
+function downloadFile(url) {
+    console.log(url);
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "blob";
+    xhr.onload = function (event) {
+        var blob = xhr.response;
+    };
+    xhr.open("GET", url);
+    xhr.send();
+}
+function getTransacciones() {
+    db.collection("TRANSACCIONES")
+        .orderBy("timeStamp", "asc")
+        .where("usuarios", "array-contains", userMail)
+        .limit(50)
+        .onSnapshot(function (querySnapshot) {
+            lastTimeStamp = Date.now();
+            querySnapshot.docChanges().forEach(function (change) {
+                doc = change.doc;
+                console.log(doc.id + " => " + doc.data());
+                console.log(doc.data().usuarios);
+                var transaccionId = doc.id;
+                if (doc.data().usuarios[0] == userMail) {
+                    db.collection("USUARIOS")
+                        .doc(doc.data().usuarios[1])
+                        .get()
+                        .then(function (doc) {
+                            var contactName = doc.data().nombre;
+                            $$("#lista-notificaciones").prepend(
+                                "<li><a class='panel-close' href='/transaccion/" +
+                                    transaccionId +
+                                    "'>de ti a " +
+                                    contactName +
+                                    "</a></li>"
+                            );
+                        });
+                } else {
+                    db.collection("USUARIOS")
+                        .doc(doc.data().usuarios[0])
+                        .get()
+                        .then(function (doc) {
+                            var contactName = doc.data().nombre;
+                            $$("#lista-notificaciones").prepend(
+                                "<li><a class='panel-close' href='/transaccion/:" +
+                                    transaccionId +
+                                    "'de " +
+                                    contactName +
+                                    " a ti</a></li>"
+                            );
+                        });
+                }
+            });
+        });
 }
